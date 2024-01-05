ee570a48f02b58734306b55eddf02ab70ff11f6c
Andrew Burgess <andrew@burgess.io>
2024-01-05 10:21:36 -0500

    dispose exapmles

---
+++ src/pool.ts
@@ -0,0 +1,65 @@
+// copied
+interface Disposable {
+  [Symbol.dispose](): void;
+}
+
+type Clearable = {
+  clear: () => void;
+}
+
+class Pool<T extends Clearable> {
+  #pool: Array<T & Disposable> = [];
+
+  constructor(factory: (p: Pool<T>) => T, count: number) {
+    while (count--) {
+      const t = factory(this);
+      const tPrime = t as T & Disposable;
+
+      tPrime[Symbol.dispose] = () => {
+        tPrime.clear();
+        this.#pool.unshift(tPrime);
+      };
+
+      this.#pool.push(tPrime);
+    }
+  }
+
+  get() {
+    const t = this.#pool.pop();
+
+    if (!t) throw new Error("pool empty");
+
+    t.clear();
+    return t;
+  }
+}
+
+class Summer {
+  #sum = 0;
+
+  clear() {
+    this.#sum = 0;
+  }
+
+  add(n: number) {
+    this.#sum += n;
+  }
+
+  result() {
+    return this.#sum;
+  }
+}
+
+const pool = new Pool(() => new Summer(), 1);
+
+const numsArr = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+];
+
+for (const nums of numsArr) {
+  using s = pool.get();
+  nums.forEach(n => s.add(n));
+  console.log(s.result());
+}
---
+++ src/transaction.ts
@@ -0,0 +1,38 @@
+class Transaction {
+  #failed = false;
+  #tasks: Array<() => Promise<void>> = [];
+
+  async run(callback: () => Promise<void>) {
+    try {
+    await callback();
+    } catch (err) {
+      this.#failed = true;
+      throw err;
+    }
+  }
+
+  enqueue(callback: () => Promise<void>) {
+    this.#tasks.push(callback);
+  }
+
+  async [Symbol.asyncDispose]() {
+    if (!this.#failed) {
+      for (const fn of this.#tasks) {
+        await fn();
+      }
+    }
+  }
+}
+
+(async () => {
+  await using t = new Transaction();
+  t.enqueue(async () => console.log('send success email'));
+
+  t.run(async () => {
+    if (1 + 1 === 2) {
+      console.log('work done');
+      return;
+    }
+    throw 'work failed'
+  });
+})();
